3bdf786f5cf32d8ef0c509f144853f827900ecec
Lucas <lucasnrgaard@gmail.com>
2025-08-03 19:56:58 +0200

    feat!: switch to tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+  entry: [
+    "./src/index.ts",
+  ],
+  format: ["cjs", "esm"],
+  clean: true,
+  dts: true,
+  treeshake: true,
+  exports: true,
+  publint: true,
+});
